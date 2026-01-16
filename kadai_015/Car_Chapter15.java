341d898286d23ed40e29ba0859b9adbedae0471d
ObaraTakumi <59912124+takumi02r@users.noreply.github.com>
2026-01-16 17:15:47 +0900

    kadai_015の再提出

--- kadai_015/Car_Chapter15.java
+++ kadai_015/Car_Chapter15.java
@@ -6,6 +6,7 @@ public class Car_Chapter15 {
 	private int speed = 10; // 速度
 	
 	public void changeGear(final int afterGear) {
+		int beforeGear = this.gear;
 		this.gear = afterGear;
 		
 		this.speed = switch (this.gear) {
@@ -16,9 +17,11 @@ public class Car_Chapter15 {
 		    case 5 -> 50;
 		    default-> 10;
 		};
+		
+		System.out.println("ギア" + beforeGear + "から" + this.gear + "に切り替えました");
 	}
 	
 	public void run() {
-		System.out.println("速度：" + speed);
+		System.out.println("速度は時速" + speed + "kmです");
 	}
 }
